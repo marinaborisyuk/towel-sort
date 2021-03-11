757dccbdc46c650406fe2d02b8d14aded6f60b8b
Marina Borisyuk <marinaborisyuk1@gmail.com>
2021-03-11 20:41:54 +0300

    feat: add creating array without sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(!matrix || matrix.length <= 0)
+  {
+    return [];
+  }
+
+  // let arr = [].concat(...matrix);
+  let arr = [];
+
+  for(let i = 0; i < matrix.length; i++)
+  { 
+    for(let j = 0; j < matrix[i].length; j++)
+    {
+      arr.push(matrix[i][j]); 
+    }
+  }
+
+  arr.sort(function (a, b) {
+    return a - b;
+});
+
+  return arr;
 }
