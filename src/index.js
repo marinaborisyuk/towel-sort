f33f38fdd182e730bbf0585e2147e6c865152445
Marina Borisyuk <marinaborisyuk1@gmail.com>
2021-03-11 22:23:48 +0300

    feat: add func reverse and push

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
   if(!matrix || matrix.length <= 0)
   {
     return [];
   }
 
-  // let arr = [].concat(...matrix);
   let arr = [];
 
   for(let i = 0; i < matrix.length; i++)
   { 
+    if(!(i % 2 == 0))
+    {
+      matrix[i].reverse();
+    }
     for(let j = 0; j < matrix[i].length; j++)
     {
       arr.push(matrix[i][j]); 
     }
-  }
-
-  arr.sort(function (a, b) {
-    return a - b;
-});
+  } 
 
   return arr;
 }
